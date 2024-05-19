e80cb83817be2921f6ec0d76fbad27495943e266
Udeme98 <Darealudeme1@gmail.com>
2024-05-19 07:03:46 +0100

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
       <Flex height={"full"}>
-        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+        <Box height={"full"} flex={5} w={"100vw"}>
           <Home />
         </Box>
       </Flex>
