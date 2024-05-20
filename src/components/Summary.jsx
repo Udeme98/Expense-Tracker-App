527911aa82d9196a385ee60f7affdea867bdc918
Udeme98 <Darealudeme1@gmail.com>
2024-05-20 06:47:45 +0100

    updated Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -33,9 +33,50 @@ const Summary = () => {
           mr={"2"}
         >
           <Heading size={"md"} mb={"4"} color={"gray.600"}>
-            Balance is 100
+            Balance is $ 100
           </Heading>
+          <Flex
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            bg={"gray.50"}
+            w="full"
+            h="100px"
+            border={"1px solid"}
+            borderColor={"gray.100"}
+          >
+            <Flex flexDirection={"column"}>
+              <Heading color={"gray.700"}>$ 100</Heading>
+              <Text color={"gray.600"}>Total Income</Text>
+            </Flex>
+          </Flex>
+          <Flex
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            bg={"gray.50"}
+            w="full"
+            h="100px"
+            border={"1px solid"}
+            borderColor={"gray.100"}
+          >
+            <Flex flexDirection={"column"}>
+              <Heading color={"gray.700"}>$ 100</Heading>
+              <Text color={"gray.600"}>Total Expense</Text>
+            </Flex>
+          </Flex>
         </Flex>
+        <Box
+          flex={1}
+          mt={"10"}
+          ml={"-90px"}
+          mr={"5"}
+          width={"300px"}
+          height={"300px"}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Heading>Charts</Heading>
+        </Box>
       </Flex>
     </Box>
   );
